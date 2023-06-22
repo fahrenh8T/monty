3bd4913a9d7978fce73c3be93fc8e36b06d32640
root <root@DESKTOP-RAM98QA>
2023-06-22 22:54:58 +0300

    main.c file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * main - The main entry point to the program
+ * @argc: argument count; Number of command line arguments
+ * @argv: argument vector; An array containing the arg.
+ * Return: zero(sucess)
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		error_output(1);
+	op_file(argv[1]);
+	free_node();
+	return (0);
+}
